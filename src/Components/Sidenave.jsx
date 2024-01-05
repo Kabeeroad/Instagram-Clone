ced5d37be54eb3306b587c4b0f127a65c64f732e
Kabeer <kabeeroad07@gmail.com>
2024-01-05 18:16:06 +0000

    adding sum metrial ui features

--- src/Components/Sidenave.jsx
+++ src/Components/Sidenave.jsx
@@ -8,13 +8,42 @@ import ChatIcon from "@mui/icons-material/Chat";
 import FavoriteBorderIcon from "@mui/icons-material/FavoriteBorder";
 import AddCircleOutlineIcon from "@mui/icons-material/AddCircleOutline";
 import MenuIcon from "@mui/icons-material/Menu";
+
 const Navigation = () => {
   return (
     <div className="sidenav">
       <img className="sidenav-logo" src="logo.png" alt="" />
       <div className="sidenavbuttons">
         <div className="side-button">
-          {/* icon */}
+          <HomeIcon />
+          <span>Home</span>
+        </div>
+        <div className="side-button">
+          <SearchIcon />
+          <span>Search</span>
+        </div>
+        <div className="side-button">
+          <ExploreIcon />
+          <span>Explore</span>
+        </div>
+        <div className="side-button">
+          <SlideshowIcon />
+          <span></span>
+        </div>
+        <div className="side-button">
+          <ChatIcon />
+          <span>home</span>
+        </div>
+        <div className="side-button">
+          <FavoriteBorderIcon />
+          <span>home</span>
+        </div>
+        <div className="side-button">
+          <AddCircleOutlineIcon />
+          <span>home</span>
+        </div>
+        <div className="side-button">
+          <MenuIcon />
           <span>home</span>
         </div>
       </div>
