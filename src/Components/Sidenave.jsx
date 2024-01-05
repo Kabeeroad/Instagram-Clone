655f61068811768447eee627f0b46d7c2bded245
Kabeer <kabeeroad07@gmail.com>
2024-01-05 18:02:05 +0000

    this is timeline components

--- src/Components/Sidenave.jsx
+++ src/Components/Sidenave.jsx
@@ -1,5 +1,13 @@
 import React from "react";
 import "./Sidenav.css";
+import HomeIcon from "@mui/icons-material/Home";
+import SearchIcon from "@mui/icons-material/Search";
+import ExploreIcon from "@mui/icons-material/Explore";
+import SlideshowIcon from "@mui/icons-material/Slideshow";
+import ChatIcon from "@mui/icons-material/Chat";
+import FavoriteBorderIcon from "@mui/icons-material/FavoriteBorder";
+import AddCircleOutlineIcon from "@mui/icons-material/AddCircleOutline";
+import MenuIcon from "@mui/icons-material/Menu";
 const Navigation = () => {
   return (
     <div className="sidenav">
