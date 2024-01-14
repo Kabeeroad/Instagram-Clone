5dbcb43cca35b62e05aba0d28bd0f0a0daaaf5af
Kabeer <kabeeroad07@gmail.com>
2024-01-14 16:57:15 +0000

    remove new foalder

--- src/Components/Sidenave.jsx
+++ src/Components/Sidenave.jsx
@@ -1,21 +1,21 @@
 import React from "react";
 import "./Sidenav.css";
-// import HomeIcon from "@mui/icons-material/Home";
-// import SearchIcon from "@mui/icons-material/Search";
-// import ExploreIcon from "@mui/icons-material/Explore";
-// import SlideshowIcon from "@mui/icons-material/Slideshow";
-// import ChatIcon from "@mui/icons-material/Chat";
-// import FavoriteBorderIcon from "@mui/icons-material/FavoriteBorder";
-// import AddCircleOutlineIcon from "@mui/icons-material/AddCircleOutline";
-// import MenuIcon from "@mui/icons-material/Menu";
-import { FaHome } from "react-icons/fa";
+import HomeIcon from "@mui/icons-material/Home";
+import SearchIcon from "@mui/icons-material/Search";
+import ExploreIcon from "@mui/icons-material/Explore";
+import SlideshowIcon from "@mui/icons-material/Slideshow";
+import ChatIcon from "@mui/icons-material/Chat";
+import FavoriteBorderIcon from "@mui/icons-material/FavoriteBorder";
+import AddCircleOutlineIcon from "@mui/icons-material/AddCircleOutline";
+import MenuIcon from "@mui/icons-material/Menu";
+
 const Navigation = () => {
   return (
     <div className="sidenav">
       <img className="sidenav-logo" src="logo.png" alt="" />
       <div className="sidenavbuttons">
         <div className="side-button">
-          <FaHome />
+          <HomeIcon />
           <span>Home</span>
         </div>
         <div className="side-button">
