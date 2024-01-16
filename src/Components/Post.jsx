04b79eb767f21ff1820e487f3244fc8287183cf8
Kabeer <kabeeroad07@gmail.com>
2024-01-16 18:33:37 +0000

    close the system

--- src/Components/Post.jsx
+++ src/Components/Post.jsx
@@ -1,9 +1,12 @@
 import React from "react";
 import "./post.css";
+import { Avatar } from "@mui/material";
 const Post = () => {
   return (
     <div className="post">
-      <div className="post-header"></div>
+      <div className="post-header">
+        <Avatar>K</Avatar>
+      </div>
       <div className="post-img"></div>
       <div className="post-footer"></div>
     </div>
