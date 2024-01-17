bf843acfd71639bc7ce61897dad3abb40b82e7dd
Kabeer <kabeeroad07@gmail.com>
2024-01-17 16:38:12 +0000

    batttery dead

--- src/Components/Post.jsx
+++ src/Components/Post.jsx
@@ -1,11 +1,16 @@
 import React from "react";
 import "./post.css";
 import { Avatar } from "@mui/material";
+import { MoreHoriz } from "@mui/icons-material";
 const Post = () => {
   return (
     <div className="post">
       <div className="post-header">
-        <Avatar>K</Avatar>
+        <div className="post-headerAuth">
+          <Avatar>K</Avatar>
+          radian_. <span>12h</span>
+        </div>
+        <MoreHoriz />
       </div>
       <div className="post-img"></div>
       <div className="post-footer"></div>
