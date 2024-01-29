54691d251dd96aa823e21102a39c4acfdfd48f94
Kabeer <kabeeroad07@gmail.com>
2024-01-29 17:52:10 +0000

    calling apis

--- src/Components/Post.jsx
+++ src/Components/Post.jsx
@@ -14,7 +14,12 @@ const Post = () => {
 
         <MoreHorizOutlined />
       </div>
-      <div className="post-img">img</div>
+      <div className="post-img">
+        <img
+          src="https://th.bing.com/th/id/R.7d87a9db161c1987ccc79e260c2af967?rik=ov%2byM6p3OU2hYA&riu=http%3a%2f%2feskipaper.com%2fimages%2fbeauty-model-girl-portrait-1.jpg&ehk=GzUKVAL%2bf%2beb2gvQtc5NZgTaYVKCqFwU5f3a54bDVoE%3d&risl=&pid=ImgRaw&r=0"
+          alt=""
+        />
+      </div>
       <div className="post-footer"></div>
     </div>
   );
